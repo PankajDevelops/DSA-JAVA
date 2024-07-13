ae79fc52661595dc1643a35ce3185952179eac55
PankajDevelops <npankaj5538@gmail.com>
2024-07-13 23:29:33 +0530

    minor updates

--- Strings/Lev-2/longSubstring.java
+++ Strings/Lev-2/longSubstring.java
@@ -1,3 +1,4 @@
+
 // package Strings.Lev-2;
 /* 
 3. Longest Substring Without Repeating Characters
@@ -17,14 +18,36 @@ Input: s = "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.*/
+import java.util.*;
 
 public class longSubstring {
 
    public static void main(String[] args) {
 
-      String str = "pwwkew";
-    
+      String str = "abcabcbb";
 
+      int left = 0;
+      int right = 0;
+
+      int maxNum = 0;
+
+
+      boolean[] arr = new boolean[256];
+
+      while (right < str.length()) {
+
+         while (arr[str.charAt(right)]) {
+            arr[str.charAt(left)] = false;
+            left++;
+         }
+
+         arr[str.charAt(right)] = true;
+         maxNum = Math.max(maxNum, right - left + 1);
+         right++;
+
+      }
+
+      System.out.println(maxNum);
    }
-    
+
 }
