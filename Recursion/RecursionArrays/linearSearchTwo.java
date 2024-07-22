e093e6170d828306612a56216fb5b5b3d8ef92e1
PankajDevelops <npankaj5538@gmail.com>
2024-07-22 20:13:16 +0530

    upadte

--- Recursion/RecursionArrays/linearSearchTwo.java
+++ Recursion/RecursionArrays/linearSearchTwo.java
@@ -4,37 +4,36 @@ import java.util.ArrayList;
 
 public class linearSearchTwo {
 
-    public static ArrayList<Integer> findIndex(int arr[], int X, int i){
+    public static ArrayList<Integer> findIndex(int arr[], int X, int i) {
 
         ArrayList<Integer> ls = new ArrayList<>();
 
-        if(i>=arr.length){
+        if (i >= arr.length) {
             return new ArrayList<Integer>();
         }
 
-        if(arr[i]==X) ls.add(i);
+        if (arr[i] == X)
+            ls.add(i);
 
-        ArrayList<Integer> smallAns = findIndex(arr, X, i+1);
+        ArrayList<Integer> smallAns = findIndex(arr, X, i + 1);
 
-        ls.addAll(smallAns); 
-
-        findIndex(arr, X, i+1);
+        ls.addAll(smallAns);
 
         return ls;
     }
 
     public static void main(String[] args) {
-        
-        int arr[] = {1,2,3,2,2,5};
+
+        int arr[] = { 1, 2, 3, 2, 2, 5 };
         int X = 2;
         int i = 0;
 
         ArrayList<Integer> ls = findIndex(arr, X, i);
 
-        for(int num : ls){
-            System.out.print(num+" ");
+        for (int num : ls) {
+            System.out.print(num + " ");
         }
 
     }
-    
+
 }
---
+++ Recursion/RecursionArrays/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+ArrayList<Integer> smallAns = findIndex(arr, X, i + 1);
+
+        // ls.addAll(smallAns);
