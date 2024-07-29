4695529cf0856dd92bf0c35de967d0103d7a735e
PankajDevelops <npankaj5538@gmail.com>
2024-07-29 13:48:03 +0530

    upadtion in sorting Techniques !

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -17,13 +17,11 @@ public class BubbleSort {
             print(arr,7);
             System.out.println("");
             
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n-1 ;j++){
-                    if(arr[j]>arr[j+1]){
-                       swap(arr,j,j+1);
-                    }
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1])
+                        swap(arr, j, j + 1);
                 }
-                
             }
             
             print(arr,7);
--- Sorting/InsertationSort.java
+++ Sorting/InsertationSort.java
@@ -47,18 +47,15 @@ public class InsertationSort {
 
             //best method
 
-            for(int i=1; i<n; i++){
+            for (int i = 1; i < n; i++) {
 
-                int curr = arr[i];
-                int prev = i-1;
+                int j = i;
 
-                while(prev>=0 && arr[prev] > curr){
-                    arr[prev+1] = arr[prev];
-                    prev--;
+                while (j >= 0 && arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    j--;
                 }
 
-                arr[prev+1] = curr;
-                
             }
 
             
