73ca95aaede5460f2bde33b99faf2dc4efbc7738
PankajDevelops <npankaj5538@gmail.com>
2024-08-24 23:29:56 +0530

    major change

--- ArraysAndMatrix/ArrayLists/class_01.java
+++ ArraysAndMatrix/ArrayLists/class_01.java
@@ -10,8 +10,8 @@ public class class_01 {
 
         Integer b = Integer.valueOf(5);
 
-        // System.out.println(a);
-        // System.out.println(b);
+        System.out.println(a);
+        System.out.println(b);
 
 
         //--------------------------//
