2c64f10d108ca3e7b51299b804d30fe26a614707
PankajDevelops <npankaj5538@gmail.com>
2024-09-19 22:33:41 +0530

    nAry Tree added

---
+++ Trees/BST/implementation.java
@@ -0,0 +1,79 @@
+package Trees.BST;
+
+class implementation {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    static Node binaryTree(Integer arr[], int idx) {
+
+        if (idx >= arr.length || arr[idx] == null) // Handling null values
+            return null;
+
+        Node temp = new Node(arr[idx]);
+        temp.left = binaryTree(arr, 2 * idx + 1);
+        temp.right = binaryTree(arr, 2 * idx + 2);
+
+        return temp;
+    }
+
+    static void preOrder(Node root) {
+
+        if (root == null)
+            return;
+
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    static void inOrder(Node root) {
+
+        if (root == null)
+            return;
+
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+
+    }
+
+    static void postOrder(Node root) {
+
+        if (root == null)
+            return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = { 11, 22, 3, null, 15, 23, null }; // Using null for empty nodes
+
+        Node root = binaryTree(arr, 0);
+
+        System.out.println("preOrder");
+        preOrder(root);
+        System.out.println("");
+
+        System.out.println("inOrder");
+        inOrder(root);
+        System.out.println("");
+
+        System.out.println("postOrder");
+        postOrder(root);
+
+    }
+
+}
---
+++ Trees/GenericTrees/nAryTree.java
@@ -0,0 +1,40 @@
+package Trees.GenericTrees;
+
+import java.util.*;
+
+public class nAryTree {
+
+    public static class Node {
+
+        int val;
+        List<Node> child;
+
+        Node(int val){
+            this.val = val;
+            child = new ArrayList<>();
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        
+        Node root = new Node(30);
+        root.child.add(new Node(5));
+        root.child.add(new Node(11));
+        root.child.add(new Node(63));
+
+        root.child.get(0).child.add(new Node(1));
+        root.child.get(0).child.add(new Node(4));
+        root.child.get(0).child.add(new Node(8));
+
+        root.child.get(1).child.add(new Node(6));
+        root.child.get(1).child.add(new Node(7));
+        root.child.get(1).child.add(new Node(15));
+
+        root.child.get(1).child.add(new Node(31));
+        root.child.get(1).child.add(new Node(55));
+        root.child.get(1).child.add(new Node(65));
+
+    }
+    
+}
