0017e95fcdb813aa4bce66650f12e3079def7aab
PankajDevelops <npankaj5538@gmail.com>
2024-10-03 21:59:26 +0530

    stack ques addition

---
+++ Stacks/Expressions/infixExpression.java
@@ -0,0 +1,15 @@
+package Stacks.Expressions;
+import java.util.*;
+
+public class infixExpression {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> ist = new Stack<>();
+        Stack<Character> cst = new Stack<>();
+
+        
+
+    }
+    
+}
