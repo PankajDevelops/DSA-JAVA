51ca8cc0e5f558b587d2576ca1068249eec047d0
PankajDevelops <npankaj5538@gmail.com>
2025-04-15 23:52:51 +0530

    update

--- ArraysAndMatrix/SlidingWindow/minSIzeSubarraySum.java
+++ ArraysAndMatrix/SlidingWindow/minSIzeSubarraySum.java
@@ -7,8 +7,8 @@ public class minSIzeSubarraySum {
     public static void main(String[] args) {
 
         /*
-         * Input: target = 7, nums = [2,3,1,2,4,3]
-         * Output: 2
+         * Input: target = 7, nums = [2,3,1,2,4,3];
+         * Output: 2 ;
          * 
          */
 
