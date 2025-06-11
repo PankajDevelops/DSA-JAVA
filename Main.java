534782a4c306ccee9176c5d42d59cc2351bd4f72
PankajDevelops <npankaj5538@gmail.com>
2025-06-11 23:43:19 +0530

    Moving to DEV

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Let's restart this game again !");
+        System.out.println("Let's restart this game again !");
+        System.out.println("Let's restart this game again !");
+        System.out.println("Let's restart this game again !");
 
     }
 
